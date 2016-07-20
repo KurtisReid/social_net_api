86798f5a2405c5fc0a67b49dcd7baac86b1b5357
Kurtis Reid <kreid8@kent.edu>
2016-07-20 16:45:23 -0400

    added function skeltons

--- inputKnowledgeItemsFunctions.js
+++ inputKnowledgeItemsFunctions.js
@@ -10,27 +10,77 @@ var API_KEY = require('./API_KEY').api_key;//acess api_key stored on another fil
 //var http = require("http");
 
 
-var Careers = ["computers", "Psychology"];//list of Careers
+var Careers = ["computer", "Psychology"];//list of Careers
+var year = "2013";
 
 var modify_inputKnowledgeItems_prefs = function (loc_state, student_status, career_field, desired_degree_level, current_degree_level, tuition_cost_ceiling)
 {
+var list;
+  list = find_in_state_colleges(loc_state, career_field);// returns a list of institutions in the state
+  list = check_for_accrediation(list);// checks for properly acredditted institutions, returns modified list of acreddited universities
+  list = check_tution(list, tuition_cost_ceiling);// checks institutions tuition cost against tuition_cost_ceiling, returns list of institutions under ceiling
+
+
+
 
 }
 
-var find_in_state_colleges = function(state_desired)
+var check_for_accrediation = function (data)
 {
-  var url = "&school.state=" + state_desired + "&_fields=school.name%2Cschool.tuition_revenue_per_fte,school.zip,2013.academics.program_percentage.computer,2013.cost.attendance.academic_year";
+  //keyword for api call: school.accreditor
+  //bad colleges
+  //Accrediting Council for Independent Colleges and Schools
+
+  //check ownership
+  //"school.ownership"
+  // 3 = for profit
+
+  //parse json data
+
+
+  //check for bad accreditors
+
+  // write good institutions to new json structure
+
+
+  // check tution costs
+
+
+
+}
+
+
+
+var find_in_state_colleges = function(state_desired, career_field)
+{
+
+  var url = "&school.state=" + state_desired + "&_fields=school.name%2Cschool.tuition_revenue_per_fte,school.zip," + year + ".academics.program_percentage." + Careers[career_field]+ "," + year + ".cost.attendance.academic_year" + ",school.accreditor,school.ownership";
   get_info_from_api(url);
 }
 
-var get_info_from_api = function (url)// sends GET request to server, expects valid information to be recived
+
+//year + ".cost.attendance.academic_year__range=.." + tuition_cost_ceiling
+
+var check_tution = function (list, tuition_cost_ceiling) {
+  //// checks institutions tuition cost against tuition_cost_ceiling, returns list of institutions under ceiling
+  //loop through list
+
+      //check if tution is below ceiling
+}
+
+
+
+var get_info_from_api = function (url, callback)// sends GET request to server, expects valid information to be recived
 {
   https.get('https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key='+API_KEY+url, (res) => {
     console.log(`Got response: ${res.statusCode}`);
     // consume response body
     res.on('data', (d) => {
         process.stdout.write(d);//writing data to screen
-        //callback(d);//returning data to calling function
+        callback(d);//returning data to calling function
+        //check_for_accrediation(d);
+
+
 
 
       });
@@ -41,4 +91,4 @@ var get_info_from_api = function (url)// sends GET request to server, expects va
   });
 }
 
-find_in_state_colleges("OH");
+find_in_state_colleges("OH", 0);
