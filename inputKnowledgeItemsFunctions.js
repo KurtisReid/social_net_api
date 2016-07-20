f5768f6319fff9d0fabbc9e283cd1d4012724946
Kurtis Reid <kreid8@kent.edu>
2016-07-20 15:16:58 -0400

    inital commit

---
+++ inputKnowledgeItemsFunctions.js
@@ -0,0 +1,12 @@
+//This will process the user input, and insert it into the inputKnowledgeItems section of the new LFST
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+
+
+var modify_inputKnowledgeItems_prefs = function ()
+{
+
+}
