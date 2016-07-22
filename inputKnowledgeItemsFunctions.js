a913dc69c7ab8c76486e6b98bbbd129390198db2
Kurtis Reid <kreid8@kent.edu>
2016-07-22 12:58:32 -0400

    working get and accreditation, get end observerd

--- inputKnowledgeItemsFunctions.js
+++ inputKnowledgeItemsFunctions.js
@@ -16,16 +16,79 @@ var year = "2013";
 var modify_inputKnowledgeItems_prefs = function (loc_state, student_status, career_field, desired_degree_level, current_degree_level, tuition_cost_ceiling)
 {
   var list;
-  list = find_in_state_colleges(loc_state, career_field);// returns a list of institutions in the state
-  list = check_for_accrediation(list);// checks for properly acredditted institutions, returns modified list of acreddited universities
-  list = check_tution(list, tuition_cost_ceiling);// checks institutions tuition cost against tuition_cost_ceiling, returns list of institutions under ceiling
+  find_in_state_colleges(loc_state, career_field, tuition_cost_ceiling, function (err, returned_json_file) {
+    if (err) throw err;
+    console.log("Colleges found");
+    //console.log("///////////////////////////list//////////////////");
+    console.log(returned_json_file);
+
+   /*check_for_accrediation(returned_json_file, function (err, ret_json_file) {
+     if (err) throw err;
+      console.log("///////////////////////////list//////////////////");
+      console.log("checked the accrediations");
+      console.log("accreddited colleges");
+      console.log(ret_json_file);
+      console.log("///////////////////////////list//////////////////");
+    });// checks for properly acredditted institutions, returns modified list of acreddited universities
+
+*/
+
+    //console.log("\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/");
+  });// returns a list of institutions in the state
 
 
+/*
+  list = check_for_accrediation(list, function (returned_json_file) {
+    console.log("checked the accrediations");
+    console.log("accreddited colleges");
+    console.log(returned_json_file);
+  });// checks for properly acredditted institutions, returns modified list of acreddited universities
+
+  list = check_tution(list, tuition_cost_ceiling, function (returned_json_file) {
+    console.log("Colleges found in the budget");
+    console.log(returned_json_file);
+  });// checks institutions tuition cost against tuition_cost_ceiling, returns list of institutions under ceiling
+*/
+  console.log("///////////////////////////list//////////////////");
+  console.log(list);
+  console.log("\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/");
 
   //show_list_of_colleges(list);//returns list of colleges
 }
 
-var check_for_accrediation = function (data)
+var get_info_from_api = function (url, callback)// sends GET request to server, expects valid information to be recived
+{
+  console.log(url);
+  var test_url = 'https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=pw4kBNcwzH7JHiCt6zpKgg5JcFwCfOjfkeyp5AJe&school.name=kent+State&_zip=44240&_fields=school.name%2Cschool.tuition_revenue_per_fte,school.zip,2013.academics.program_percentage.computer,2013.cost.attendance.academic_year,school.accreditor,school.ownership';
+  var data;
+  https.get(test_url,function(res) {
+      var body = '';
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {//signals end of call
+        console.log("????????????????????????????????")
+
+        console.log(body);
+
+
+        check_for_accrediation(body, function (err, ret_json_file) {
+          if (err) throw err;
+           console.log("///////////////////////////list//////////////////");
+           console.log("checked the accrediations");
+           console.log("accreddited colleges");
+           console.log(ret_json_file);
+           console.log("///////////////////////////list//////////////////");
+         });// checks for properly acredditted institutions, returns modified list of acreddited universities
+      });
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+
+
+}
+
+var check_for_accrediation = function (data, callback)
 {
   //keyword for api call: school.accreditor
   //bad colleges
@@ -38,35 +101,67 @@ var check_for_accrediation = function (data)
   //parse json data
   var parsed_document;
   parsed_document = JSON.parse(data);//parsing the json document
+  console.log("?????????????????????????? check_for_accrediation ??????????????????");
+  console.log(data);
+
+
+  var accredited_institutions = {schools: [
+
+  ]};
+  var count = 0;
+console.log("????????????????????????????????")
+  console.log(parsed_document.results);
 
   //check for bad accreditors
-  for (var i = 0; i < parsed_document.results.length; i++)
+  for (var i = 0; i < parsed_document.results; i++)
   {
     if(parsed_document.results[i]['school.accreditor'] != "Accrediting Council for Independent Colleges and Schools")
     {
       //add institutions to new json structure
+      accredited_institutions.schools.push(parsed_document.results[i]);
+
+
     }
   }
 
   //return json structure
 
+  /*list = check_tution(accredited_institutions, tuition_cost_ceiling, function (returned_json_file) {
+    console.log("Colleges found in the budget");
+    console.log(returned_json_file);
+  });// checks institutions tuition cost against tuition_cost_ceiling, returns list of institutions under ceiling
+  //callback(accredited_institutions);*/
+
+callback(null, accredited_institutions);
 
 
 }
 
 
 
-var find_in_state_colleges = function(state_desired, career_field)
+var find_in_state_colleges = function(state_desired, career_field, tuition_cost_ceiling, callback)
 {
 
   var url = "&school.state=" + state_desired + "&_fields=school.name%2Cschool.tuition_revenue_per_fte,school.zip," + year + ".academics.program_percentage." + Careers[career_field]+ "," + year + ".cost.attendance.academic_year" + ",school.accreditor,school.ownership";
-  get_info_from_api(url);
+  var data;
+  get_info_from_api(url, function (err, ret_data) {
+    if (err) throw err;
+    console.log(ret_data);
+    data = ret_data;
+  });
+console.log("///////////////////////////////////////////////////////////")
+
+  console.log(data);
+console.log("///////////////////////////////////////////////////////////")
+  callback(data);
+
+
 }
 
 
 
 
-var check_tution = function (list, tuition_cost_ceiling)
+var check_tution = function (list, tuition_cost_ceiling, callback)
 {
   //// checks institutions tuition cost against tuition_cost_ceiling, returns list of institutions under ceiling
   //loop through list
@@ -75,27 +170,11 @@ var check_tution = function (list, tuition_cost_ceiling)
       // if (tution < tuition_cost_ceiling || tution = tuition_cost_ceiling) add to list
 
   //return mondified list
+  callback(list);
 }
 
 
 
-var get_info_from_api = function (url, callback)// sends GET request to server, expects valid information to be recived
-{
-  https.get('https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key='+API_KEY+url, (res) => {
-    console.log(`Got response: ${res.statusCode}`);
-    // consume response body
-    res.on('data', (d) => {
-        process.stdout.write(d);//writing data to screen
-        callback(d);//returning data to calling function
-        //check_for_accrediation(d);
 
 
-
-
-      });
-
-    res.resume();
-  }).on('error', (e) => {
-    console.log(`Got error: ${e.message}`);
-  });
-}
+modify_inputKnowledgeItems_prefs("OH", "full",0, "Bachelor", "HS", "20000");
