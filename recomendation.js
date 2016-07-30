3b7bf943b98dcc612cdb960f8f193ad08fc095c7
Kurtis Reid <kreid8@kent.edu>
2016-07-30 15:10:16 -0400

    sets reccomendation objs values

--- recomendation.js
+++ recomendation.js
@@ -1,6 +1,7 @@
 // recommendation is selected
 
 //include state_change
+var helpers = require('./state_change');
 
 var reccoemndation_obj = {
 "OutputKnowledgeItemsID": null,
@@ -9,9 +10,26 @@ var reccoemndation_obj = {
 
 
 
+module.exports =
+{
 
-//makes recommendation object by mapping OutputKnowledgeItemsID and inputKnowledgeItemsID
+  //makes recommendation object by mapping OutputKnowledgeItemsID and inputKnowledgeItemsID
+  var set_reccomendation_obj = function (outputID, inputID, callback)
+  {
+    var rec_obj = {
+    "OutputKnowledgeItemsID": null,
+    "inputKnowledgeItemsID": null
+    };
+    rec_obj.OutputKnowledgeItemsID = outputID;
+    rec_obj.inputKnowledgeItemsID = inputID;
+
+    callback(null, rec_obj);//returns rec_obj
+  };//end of set_reccomendation_obj
+
+  doc_call = function (reccomendation_obj, learning_states_hash_table, callback,)
 
 
 
-//calls doc_call from state_change.js
+  //calls doc_call from state_change.js
+
+};//end of module.exports
