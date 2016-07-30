52a431fdc90b226471648e145ca4e234d5d27074
Kurtis Reid <kreid8@kent.edu>
2016-07-30 15:27:15 -0400

    added learning states hashtable

---
+++ learning_states_hashtable.js
@@ -0,0 +1,8 @@
+//file that stores the hashtable that strores the learningStates
+
+var HashTable = require('hashtable');// hashtable to orginize states
+
+module.exports =
+{
+  var learning_states_hash_table = new HashTable();// hash table in which the states will be stored in
+};//end of module.exports
