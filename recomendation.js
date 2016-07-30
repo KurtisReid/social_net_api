da6d88f3aadf26904fcfe497a630ff1714405c54
Kurtis Reid <kreid8@kent.edu>
2016-07-30 12:51:54 -0400

    init commit

---
+++ recomendation.js
@@ -0,0 +1,17 @@
+// recommendation is selected
+
+//include state_change
+
+var reccoemndation_obj = {
+"OutputKnowledgeItemsID": null,
+"inputKnowledgeItemsID": null
+};
+
+
+
+
+//makes recommendation object by mapping OutputKnowledgeItemsID and inputKnowledgeItemsID
+
+
+
+//calls doc_call from state_change.js
