5d25eb4379ea9dcfd6b6b9ee7b3f573295f96ada
Kurtis Reid <kreid8@kent.edu>
2016-08-01 15:51:55 -0400

    changed syntax to have hashtable initialized with a value

--- learning_states_hashtable.js
+++ learning_states_hashtable.js
@@ -1,8 +1,7 @@
 //file that stores the hashtable that strores the learningStates
 
 var HashTable = require('hashtable');// hashtable to orginize states
-
-module.exports =
-{
-  learning_states_hash_table : new HashTable()// hash table in which the states will be stored in
-};//end of module.exports
+var value = { "inputKnowledgeItemsID": null, "learningStateID": null, "OutputKnowledgeItemsID": null };
+var learning_states_hash_table = new HashTable();// hash table in which the states will be stored in
+learning_states_hash_table.put('init', value);
+module.exports = learning_states_hash_table;
