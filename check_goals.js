b55c192f47b068ceeb6a7f1f3f3dab04b41049d0
Kurtis Reid <kreid8@kent.edu>
2016-10-31 13:20:30 -0400

    inital comit

---
+++ check_goals.js
@@ -0,0 +1,6 @@
+
+//reads mock file
+
+// checks if requirments are met for each class
+// if requirments are met, or CoursesReq = null, then add to learningState
+// if requirments are not met, then it adds it to the list of todos/steps
