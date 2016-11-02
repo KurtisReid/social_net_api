28a26596159e9308b508a4cdf740b151b3cd03c2
Kurtis Reid <kreid8@kent.edu>
2016-11-02 12:48:26 -0400

    iterates through array

--- check_goals.js
+++ check_goals.js
@@ -7,28 +7,37 @@
 var fs = require("fs");
 var input = fs.readFileSync('CSDegree.json', 'utf8');
 var obj = JSON.parse(input);
-var mock_progress = '{CourseNumber": "10051",}';//mock of courses already taken.
+
+var mock_progress = JSON.parse(fs.readFileSync('classesTaken.json', 'utf8'));//mock of courses already taken.
 var LFST = fs.readFileSync('MockLFST.json', 'utf8');
 var mockLFST = JSON.parse(LFST);
 
 
 var check_requirments = function (classesTaken, classToTake, studentLFST, callback)
 {
-  var i = 1;//temp
+  //var i = 1;//temp
   //var js = JSON.parse(classToTake);
-  console.log(classToTake.CoursesReq[i].PreReqCourseNum);
-  if (classesTaken.CourseNumber == classToTake.CoursesReq[i].PreReqCourseNum || classToTake.CoursesReq.PreReqCourseNum == null)//requirments are met
+  console.log(classesTaken.classesTaken[0]);
+  //console.log(classToTake.CoursesReq[i].PreReqCourseNum);
+  for (var i = 0; i < classesTaken.classesTaken.length; i++)
   {
-    //add to learning state
-    studentLFST.learningState.push(classToTake.CoursesReq[i]);//adding class taken to array
-    callback(null);
+    if (classesTaken.classesTaken[0].CourseNumber == classToTake.CoursesReq[i].PreReqCourseNum || classToTake.CoursesReq.PreReqCourseNum == null)//requirments are met
+    {
+      //add to learning state
+      console.log(classesTaken.classesTaken[0].CourseNumber + "==" + classToTake.CoursesReq[i].PreReqCourseNum);
+      console.log("requirments met");
+      studentLFST.learningState.push(classToTake.CoursesReq[i]);//adding class taken to array
+      callback(null);
+    }
+    else
+    {
+      // add to OutputKnowledgeItems
+      console.log("need to take class");
+      studentLFST.OutputKnowledgeItems.push(classToTake.CoursesReq[i]);//adding needed class to array
+      callback(null);
+    }
   }
-  else
-  {
-    // add to OutputKnowledgeItems
-    studentLFST.OutputKnowledgeItems.push(classToTake.CoursesReq[i]);//adding needed class to array
-    callback(null);
-  }
+
 
 }
 
