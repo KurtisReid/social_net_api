27254efb240ab4ac31157b938d20b455cfc0b9a3
Kurtis Reid <kreid8@kent.edu>
2016-11-05 13:57:53 -0400

    api call iterate in order

--- rate_my_professor.js
+++ rate_my_professor.js
@@ -1,13 +1,14 @@
 //takes professors name from proposed list of courses and sends them to rate my professor, which then returns the result
 
 var rmp = require("rmp-api");//rate my professor api
-
+var i = 0;
 
 var fs = require("fs");
 var input = fs.readFileSync('CSDegree.json', 'utf8');//read class info from file
 var class_list = JSON.parse(input);//parsed list of classes
 
 var callback = function(professor) {
+  i++;
   if (professor === null) {
     console.log("No professor found.");
     return;
@@ -21,6 +22,12 @@ var callback = function(professor) {
   console.log("Chili: " + professor.chili);
   console.log("URL: " + professor.url);
   console.log("First comment: " + professor.comments[0]);
+  if (i < class_list.CoursesReq.length)
+  {
+    rmp.get(class_list.CoursesReq[i].Professor, callback);
+  }
+
 };
 
-rmp.get(class_list.CoursesReq[0].Professor, callback);
+rmp.get(class_list.CoursesReq[i].Professor, callback);//cycle through and print out all professors
+//rmp.get(class_list.CoursesReq[0].Professor, callback);
